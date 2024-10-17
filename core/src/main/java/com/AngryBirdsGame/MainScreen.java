561e03d21971e55d0e590da45fa8906c5f3c364a
Bhavikgarg302005 <147907427+Bhavikgarg302005@users.noreply.github.com>
2024-10-17 12:04:08 +0530

    ADDING STATIC GUI

--- core/src/main/java/com/AngryBirdsGame/MainScreen.java
+++ core/src/main/java/com/AngryBirdsGame/MainScreen.java
@@ -2,23 +2,47 @@ package com.AngryBirdsGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 import java.awt.*;
 
 
 public class MainScreen implements Screen {
-    private final Main game;
-    public MainScreen(final Main main) {
-        this.game=main;
+    private final LoadScreen game;
+    private Stage stage;
+    private Sprite background;
+    private ImageButton newGameButton;
+    private ImageButton LoadSavedGameButton;
+    private ImageButton ExitButton;
+
+    public MainScreen(final LoadScreen main) {
+        this.game = main;
+        this.stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        background=new Sprite(new Texture("img.png"));
+        background.setPosition(0, 0);
+        background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        newGameButton=new ImageButton(new SpriteDrawable(new Sprite(new Texture("img.png"))));
+        LoadSavedGameButton=new ImageButton(new SpriteDrawable(new Sprite(new Texture("img.png"))));
+        ExitButton=new ImageButton(new SpriteDrawable(new Sprite(new Texture("img.png"))));
+        stage.addActor(newGameButton);
+        stage.addActor(LoadSavedGameButton);
+        stage.addActor(ExitButton);
+
+    }
+    public void update(float delta) {
+        this.stage.act(delta);
     }
 
-    private Texture background;
-    private Texture newGameButton;
-    private Texture LoadSavedGameButton;
-    private Texture ExitButton;
+
     @Override
     public void show() {
 
@@ -26,12 +50,14 @@ public class MainScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+       update(delta);
+       stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+             stage.getViewport().update(width, height, false);
     }
 
     @Override
